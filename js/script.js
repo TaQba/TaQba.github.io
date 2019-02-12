5b437c5a33b3f4f58995eb92294eeda4dd6b9f02
jakub.tadych@gmail.com <!Madzia1980>
2019-02-12 21:54:44 +0000

    task 19.1- fix

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
   // const apiRoot = 'http://localhost:8080/v1/task/';
   const apiRoot = 'https://peaceful-wave-35421.herokuapp.com/v1/task/';
 
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  // const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const trelloApiRoot = 'https://peaceful-wave-35421.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
